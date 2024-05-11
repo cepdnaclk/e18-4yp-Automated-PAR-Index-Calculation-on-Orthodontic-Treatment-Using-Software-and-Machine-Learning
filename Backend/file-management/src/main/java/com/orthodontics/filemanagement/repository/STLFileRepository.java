badf4890c8b9521108a2e6b2b0ab2e1d1fe719d8
anushanga pavith <anushangapavith2@gmail.com>
2024-05-12 00:05:31 +0530

    Removed backend

--- Backend/file-management/src/main/java/com/orthodontics/filemanagement/repository/STLFileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.orthodontics.filemanagement.repository;
-
-import com.orthodontics.filemanagement.model.STLFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface STLFileRepository extends JpaRepository<STLFile, Long> {
-    long count();
-}
