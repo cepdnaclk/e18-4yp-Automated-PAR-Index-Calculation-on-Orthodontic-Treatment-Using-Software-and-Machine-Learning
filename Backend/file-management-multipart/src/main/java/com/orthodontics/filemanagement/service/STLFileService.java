630e509c85bd75d113011a6988e9aa8e31dc93b3
anushanga pavith <anushangapavith2@gmail.com>
2024-05-13 20:06:16 +0530

    Updated backend

--- Backend/file-management-multipart/src/main/java/com/orthodontics/filemanagement/service/STLFileService.java
+++ Backend/file-management-multipart/src/main/java/com/orthodontics/filemanagement/service/STLFileService.java
@@ -5,13 +5,11 @@ import com.orthodontics.filemanagement.model.STLFiles;
 import com.orthodontics.filemanagement.repository.STLFileRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Service
 @RequiredArgsConstructor
@@ -19,25 +17,41 @@ import java.nio.file.Paths;
 public class STLFileService {
 
     private final STLFileRepository STLFileRepository;
-    private static final String UPLOAD_DIR = "C:/Users/META/OneDrive/Documents/STLData";
+
+    @Value("${app.fileLocation}")
+    private String FOLDER_PATH;
 
     public void createSTLFile(PatientRegisterRequest patientRegisterRequest, Long patient_id) throws IOException {
+
         STLFiles stlFiles = STLFiles.builder()
                 .patient_id(patient_id)
-                .prep(storeFile(patientRegisterRequest.getPrep_file()))
-                .buccal(storeFile(patientRegisterRequest.getBuccal_file()))
-                .opposing(storeFile(patientRegisterRequest.getOpposing_file()))
                 .build();
 
         STLFileRepository.save(stlFiles);
+        Long stl_id = stlFiles.getStl_id();
+
+        String prepFileName = stl_id + "_prep_" + patientRegisterRequest.getPrep_file().getOriginalFilename();
+        String buccalFileName = stl_id + "_buccal_" + patientRegisterRequest.getBuccal_file().getOriginalFilename();
+        String opposingFileName = stl_id + "_opposing_" + patientRegisterRequest.getOpposing_file().getOriginalFilename();
+
+        String prepFilePath = storeFile(patientRegisterRequest.getPrep_file(), prepFileName);
+        String buccalFilePath = storeFile(patientRegisterRequest.getBuccal_file(), buccalFileName);
+        String opposingFilePath = storeFile(patientRegisterRequest.getOpposing_file(), opposingFileName);
+
+        stlFiles.setPrep(prepFilePath);
+        stlFiles.setOpposing(opposingFilePath);
+        stlFiles.setBuccal(buccalFilePath);
+
+        STLFileRepository.save(stlFiles);
+
         log.info("STLFile created: {}", stlFiles);
     }
 
-    private String storeFile(MultipartFile file) throws IOException {
+    private String storeFile(MultipartFile file, String fileName) throws IOException {
         if (file != null && !file.isEmpty()) {
-            Path targetLocation = Paths.get(UPLOAD_DIR, file.getOriginalFilename());
-            Files.copy(file.getInputStream(), targetLocation);
-            return targetLocation.toAbsolutePath().toString();
+            String filePath = FOLDER_PATH + fileName;
+            file.transferTo(new File(filePath));
+            return filePath;
         }
         return null;
     }
