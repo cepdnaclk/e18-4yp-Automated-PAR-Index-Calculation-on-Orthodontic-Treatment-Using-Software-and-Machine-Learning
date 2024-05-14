134d31bf9e5bbee63813ded9b76f7c6baf813a6b
chamudij <chamudij99@gmail.com>
2024-05-14 23:13:40 +0530

    File Loading Feature added(Frontend)

--- Frontend/main.py
+++ Frontend/main.py
@@ -22,7 +22,7 @@ class MainWindow(QMainWindow):
 
         self.btn_load = QPushButton("Load STL")
 
-        self.label = QLabel('Select the filetype:', self)
+        self.label_filetype = QLabel('Select the file type:')
         self.fileTypeComboBox = QComboBox()
         self.fileTypeComboBox.addItems(
             ["Upper Anterior Segment", 
@@ -33,6 +33,7 @@ class MainWindow(QMainWindow):
 
         self.fileTypeComboBox.currentIndexChanged.connect(self.update_file_type)
 
+        self.label_measurementtype = QLabel('Select the measurement type:')
         self.measurementTypeComboBox = QComboBox()
         self.measurementTypeComboBox.addItems(
             ["Upper Anterior Segment Alignment", 
@@ -87,10 +88,14 @@ class MainWindow(QMainWindow):
         }
         """
 
-        for btn in [self.btn_register,self.btn_load,self.fileTypeComboBox,self.measurementTypeComboBox, self.btn_save_json, self.btn_reset, self.btn_undo, self.btnSave]:
-            btn.setStyleSheet(button_style)
+        for btn in [self.btn_register,self.label_filetype,self.fileTypeComboBox,self.btn_load,self.label_measurementtype,self.measurementTypeComboBox, self.btn_save_json, self.btn_reset, self.btn_undo, self.btnSave]:
+            if (btn == self.label_filetype or btn == self.label_measurementtype):
+                btn.setStyleSheet("color: white; font-size: 14px; margin: 5px;")
+            else:
+                btn.setStyleSheet(button_style)
             self.buttonPanel.addWidget(btn)
 
+        self.buttonPanel.addSpacing(10)
         self.btn_register.clicked.connect(self.open_register_window)
         self.btn_load.clicked.connect(lambda: load_stl(self))
         self.btn_save_json.clicked.connect(lambda: save_to_json(self))
@@ -122,7 +127,7 @@ class MainWindow(QMainWindow):
         self.register_window.show()  # Show the RegisterWindow
     
     def handle_data_from_register(self, data):
-        print("Received data:", data)
+        self.file_data = data
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
