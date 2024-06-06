4bae6939ac392b8a9519de0aaa8b91d99d939a83
anushanga pavith <anushangapavith2@gmail.com>
2024-06-06 21:22:08 +0530

    Added new API endpoints

--- Backend/file-management-multipart/src/main/java/com/orthodontics/filemanagement/service/PatientService.java
+++ Backend/file-management-multipart/src/main/java/com/orthodontics/filemanagement/service/PatientService.java
@@ -1,11 +1,17 @@
 package com.orthodontics.filemanagement.service;
 
 import com.orthodontics.filemanagement.dto.PatientRegisterRequest;
+import com.orthodontics.filemanagement.dto.PatientsResponse;
 import com.orthodontics.filemanagement.model.Patient;
 import com.orthodontics.filemanagement.repository.PatientRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +28,39 @@ public class PatientService {
 
         patientRepository.save(patient);
         return patient.getPatient_id();
+    }
+
+    public List<PatientsResponse> getAllPatients() {
+        List<Patient> allPatients =  patientRepository.findAll();
+        List<PatientsResponse> patients = new java.util.ArrayList<>(List.of());
+
+        for (Patient patient : allPatients) {
+            PatientsResponse finalPatient= PatientsResponse.builder()
+                                                .patient_id(patient.getPatient_id())
+                                                .name(patient.getName())
+                                                .build();
+
+            if(Objects.equals(patient.getTreatment_status(), "Pre Treatment")) {
+                finalPatient.setPre_PAR_score(10.0);
+                for (Patient patient1 : allPatients) {
+                     if(patient.getName().equals(patient1.getName()) && Objects.equals(patient1.getTreatment_status(), "Post Treatment")) {
+                        finalPatient.setPost_PAR_score(20.0);
+                        allPatients.remove(patient1);
+                    }
+                }
+            }
+            else if(Objects.equals(patient.getTreatment_status(), "Post Treatment")) {
+                finalPatient.setPost_PAR_score(20.0);
+                for (Patient patient1 : allPatients) {
+                    if(patient.getName().equals(patient1.getName()) && Objects.equals(patient1.getTreatment_status(), "Pre Treatment")) {
+                        finalPatient.setPre_PAR_score(10.0);
+                        allPatients.remove(patient1);
+                    }
+                }
+            }
+
+            patients.add(finalPatient);
+        }
+        return patients;
     }
 }
